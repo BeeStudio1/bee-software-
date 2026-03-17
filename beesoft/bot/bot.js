c200f880e223034b9169739fb0887f521c064922
Bee's stuff <tg999hqp2g@privaterelay.appleid.com>
2026-03-17 16:46:44 +0000

    Ajouter le fichier bot.js manquant

---
+++ beesoft/bot/bot.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`✅ Bot Discord connecté en tant que ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('Pong! 🏓');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
